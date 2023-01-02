7fd6c6cdb28aa00f19d77fb553f130920b29c365
Kween <kweenyoallhiada@gmail.com>
2023-01-02 11:40:16 -0800

    Completed lessons

---
+++ FrontendMasters/BrianHolt/Conditionals/conditionals.js
@@ -0,0 +1,19 @@
+const x = 5;
+const y = 10;
+const z = 11;
+
+if (x !== y) {
+    console.log('these are not equal');
+}
+
+if (x === y && y === z && isTrue) {
+    console.log('these are all equal');
+}
+
+if (isTrue || x === z) {
+    console.log('this is an or statement');
+}
+
+if (isTrue || (x === z && y === z)) {
+    console.log('this is an or statement');
+}
---
+++ FrontendMasters/BrianHolt/ObjectsAndArrays/person.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Kween Yoallhiada",
+    city: "Houston",
+    state: "TX",
+    favoriteFood: "🍤",
+    wantsShrimpRightNow: true,
+    numberOfShrimpWanted: 10
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person["name"]); // same as line above
