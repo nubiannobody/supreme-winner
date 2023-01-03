6df2bb7cf77e04faea819b420977e8f107af9fa9
Kween <kweenyoallhiada@gmail.com>
2023-01-02 22:30:25 -0800

    Added testing folder

---
+++ FrontendMasters/BrianHolt/Testing/listOfNames.js
@@ -0,0 +1,7 @@
+function getListOfNames(list) {
+    // list is an array of objects that looks like this
+    // { name: "Some Person", jobTitle: "Boss Person", age: 30 }
+    // return a list of all the workers' names
+  
+    return ["Bobby Baratheon", "Jonny Snow", "Dani Targaryen"];
+  }
