ff1877458c336e0f3f33682a55045f804f0c69c9
Kween <kweenyoallhiada@gmail.com>
2023-01-10 13:39:28 -0800

    Add a new file

---
+++ FrontendMasters/BrianHolt/DogGallery/static/gallery/images/gallery.js
@@ -0,0 +1,11 @@
+const prevBtn = document.querySelector('prev');
+const nextBtn = document.querySelector('next');
+
+prevBtn.addEventListener('click', function() {
+    //we'll come back to you
+});
+
+nextBtn.addEventListener('click', function() {
+
+});
+
