0e885a6f4cd85fb0b3ec0dbea2554d7424d30dba
Kween <kweenyoallhiada@gmail.com>
2023-01-10 13:56:35 -0800

    Add a new file

--- FrontendMasters/BrianHolt/DogGallery/gallery.js
+++ FrontendMasters/BrianHolt/DogGallery/gallery.js
@@ -1,6 +1,7 @@
-const prevBtn = document.querySelector('prev');
-const nextBtn = document.querySelector('next');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
 const galleryImgs = document.querySelectorAll('.gallery-img');
+let currentlySelected = 0;
 
 prevBtn.addEventListener('click', function() {
     //we'll come back to you
@@ -10,5 +11,6 @@ nextBtn.addEventListener('click', function() {
     galleryImgs[currentlySelected].classList.remove("active");
     currentlySelected++; //currentlySelected = currentlySelected + 1;
     galleryImgs[currentlySelected].classList.add("active");
+    prevBtn.disabled = false;
 });
 
