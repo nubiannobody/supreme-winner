a274d0816df1096305777a6bc750eba2bfa73478
Kween <kweenyoallhiada@gmail.com>
2023-01-10 15:21:36 -0800

    Add a new file

--- gallery-2/gallery.js
+++ gallery-2/gallery.js
@@ -9,3 +9,28 @@ new Swiper(".swiper-container", {
         prevEl: ".swiper-button-prev"
     }
 });
+
+// navigation physics
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.brand');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
