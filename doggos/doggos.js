f20d52cb332b7376ea22f89f6b280d76876e49ce
Kween <kweenyoallhiada@gmail.com>
2023-01-11 17:44:57 -0800

    Add API

--- doggos/doggos.js
+++ doggos/doggos.js
@@ -2,8 +2,10 @@ const BREEDS_URL = 'https://dog.ceo/api/breeds/image/random';
 
 fetch(BREEDS_URL)
 .then(function(response) {
-
+    return response.json();
 })
 .then(function(data){
     console.log(data);
 })
+
+console.log("which will log first");
