c29db83cbcae1a12ab603a6277cffb277a8a9c9a
Kween <kweenyoallhiada@gmail.com>
2023-01-11 20:25:34 -0800

    RAdd event

--- doggos/doggos.js
+++ doggos/doggos.js
@@ -17,11 +17,26 @@
 // document.querySelector('.add-doggo').addEventListener("click", addDoggo);
 
 const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const select = document.querySelector('.breeds');
 
 fetch(BREEDS_URL)
 .then(function (response){
     return response.json();
 })
 .then(function (data) {
-    console.log(data)
+    //console.log(data)
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+
+
+    for (let i = 0; i < breedsArray.length; i++) {
+        const option = document.createElement('option');
+        option.value = breedsArray[i];
+        option.innerText = breedsArray[i];
+        select.appendChild(option);
+    }
+})
+
+select.addEventListener("change", function(event) {
+    console.log(event.target.value);
 })
