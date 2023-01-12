abc11768d09ea4a8254b135067713c8fe69c68b0
Kween <kweenyoallhiada@gmail.com>
2023-01-11 18:34:53 -0800

    Add API

--- doggos/doggos.js
+++ doggos/doggos.js
@@ -1,16 +1,18 @@
 const BREEDS_URL = 'https://dog.ceo/api/breeds/image/random';
 
-
-fetch(BREEDS_URL)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data){
-    const img = document.createElement('img');
-    img.src = data.message;
-    img.alt = 'Cute doggo';
-
-    document.querySelector('.doggos').appendChild(img);
-
-    console.log("This does not work!")
-})
+function addDoggo () {
+    fetch(BREEDS_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        const img = document.createElement('img');
+        img.src = data.message;
+        img.alt = 'Cute doggo';
+    
+        document.querySelector('.doggos').appendChild(img);
+    
+        console.log("This does not work!")
+    })
+    
+}
