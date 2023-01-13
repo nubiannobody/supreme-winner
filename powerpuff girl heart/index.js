2bc2980a860f64c1cc5321125f4614d23c577e6a
Kween <kweenyoallhiada@gmail.com>
2023-01-12 23:22:14 -0800

    Added setup files

--- powerpuff girl heart/index.js
+++ powerpuff girl heart/index.js
@@ -9,8 +9,25 @@ function useTheHeart(n) {
     use.n = n;
     use.setAttributeNS(SVG_XLINK, 'xlink:href', '#heart');
     use.setAttributeNS(null, 'transform', `scale(${use.n})`);
+    use.setAttributeNS(null, 'fill', colors[n%colors.length]);
     use.setAttributeNS(null, 'x', -69);
     use.setAttributeNS(null, 'y', -69);
     use.setAttributeNS(null, 'width', 138);
     use.setAttributeNS(null, 'height', 138);
 }
+
+for(let n = 18; n >= 0; n--) {useTheHeart(n)}
+
+function Frame() { 
+    window.requestAnimationFrame(Frame);
+    for(let i = 0; i< heartsRy.length; i++){
+        if(heartsRy[i].n < 18) {heartsRy[i].n +=.01
+        }else {
+            heartsRy[i].n = 0;
+            heartsRy.appendChild(heartsRy[i])
+        }
+        heartsRy[i].setAttributeNS(null, 'transform', `scale(${heartsRy[i].n})`);
+    }
+}
+
+Frame()
